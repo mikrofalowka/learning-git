f09c8d3b92ddaa7235646294916cbf63a685320f
Jan Kuncewicz <jankuncewicz@gmail.com>
2024-07-17 16:16:15 +0200

    Commit message

---
+++ learning_git/main2.py
@@ -0,0 +1,15 @@
+import sys
+
+
+slowo = "kajak" #input("Jakie slowo chcesz sprawdzic? ")
+same = 0
+for i in range(len(slowo)):
+    if slowo[i] == slowo[-i-1]:
+        print('yes')
+        same +=1
+    else:
+        print('no')
+    print("-------")
+
+if same == len(slowo):
+    print(f"{slowo} jest palindromen")
